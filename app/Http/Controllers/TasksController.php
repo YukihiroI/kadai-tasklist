ffd0eacb73fee5fa02815f508b363e9664fdf1fa
yukihiro.iwata <yukihiro.iwata@rakuten.com>
2018-06-14 00:55:14 +0000

    Procfile

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -14,12 +14,18 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        $tasks = Task::all();
 
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
+    {
+        if (\Auth::check()) {
+            $tasks =Task::all();
+            
+            return view('tasks.index',[
+            'tasks' =>$tasks,
+            ]);
+        }
+        else {
+            return view('welcome');
+        }
     }
 
     /**
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
     @if (Auth::check())
-        <?php $user = Auth::user(); ?>
-        {{ $user->name }}
+      Route::get('/', TasksController@index)
     @else
         <div class="center jumbotron">
             <div class="text-center">
